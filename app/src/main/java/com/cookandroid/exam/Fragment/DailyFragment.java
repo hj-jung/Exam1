9291c934e7898e4bc33791b377a6acd89abfd6cd
Your Name <you@example.com>
2022-05-03 17:36:23 +0900

    UI 4차

--- app/src/main/java/com/cookandroid/exam/Fragment/DailyFragment.java
+++ app/src/main/java/com/cookandroid/exam/Fragment/DailyFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -14,6 +15,13 @@ import androidx.fragment.app.FragmentTransaction;
 import com.cookandroid.exam.Activity.ScheduleCheckAcitivity;
 import com.cookandroid.exam.Activity.ScheduleUpdateActivity;
 import com.cookandroid.exam.R;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class DailyFragment extends Fragment {
 
@@ -24,6 +32,31 @@ public class DailyFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_daily, container, false);
 
         ImageButton plus = (ImageButton) view.findViewById(R.id.daily_plus);
+        TextView day = (TextView) view.findViewById(R.id.day);
+
+        //상단 현재 날짜
+        Date date = Calendar.getInstance().getTime();
+        SimpleDateFormat format = new SimpleDateFormat("yyyy. MM. dd", Locale.getDefault());
+        String strday = format.format(date);
+
+        Date current = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(current);
+        int dayofWeekNumber = calendar.get(Calendar.DAY_OF_WEEK);
+        String weekday="SUN";
+
+        switch (dayofWeekNumber){
+            case 1: weekday="SUN";    break;
+            case 2: weekday="MON";    break;
+            case 3: weekday="TUE";    break;
+            case 4: weekday="WED";    break;
+            case 5: weekday="THU";    break;
+            case 6: weekday="FRI";    break;
+            case 7: weekday="SAT";    break;
+        }
+
+        day.setText(strday+"  "+weekday);
+
 
         //->일정등록페이지로 이동
         plus.setOnClickListener(new View.OnClickListener() {
